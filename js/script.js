b509d2fbdfe6622b052d9c33ecda5559a3cf5d6d
Kririll <hoprosx@gmail.com>
2022-02-25 19:54:07 +0500

    Second practice

--- js/script.js
+++ js/script.js
@@ -10,12 +10,26 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", " ");
-const c = +prompt("На сколько вы его оцениваете?", "");
-const b = prompt("Один из последних просмотренных фильмов?", " ");
-const d = +prompt("На сколько вы его оцениваете?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", " "),
+    b = +prompt("На сколько вы его оцениваете?", "");
+  personalMovieDB.movies[a] = b;
 
-personalMovieDB.movies[a] = c;
-personalMovieDB.movies[b] = d;
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  alert("Просмотрено довльно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  alert("Вы посмотрели достаточно много фильмов");
+} else {
+  alert("Ошибка");
+}
 
 console.log(personalMovieDB);
